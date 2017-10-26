58d4144d153644b2caa4d8810c48a1b80dc90125
FERDEDDO <FERDEDDO@GMAIL.com>
2017-10-26 16:10:27 +0200

    first commit

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,18 @@
+class CoursesController < ApplicationController
+
+def index
+end
+
+def dessin
+end
+
+def troll
+end
+
+def pirate
+end
+
+def pokemon
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+root 'courses#index'
+get "/dessin", to: "courses#dessin"
+get "/troll", to: "courses#troll"
+get "/pirate", to: "courses#pirate"
+get "/pokemon", to: "courses#pokemon"
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
